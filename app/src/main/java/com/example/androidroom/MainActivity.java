6a4dd0986d6c5e7eca44125d904af7be22164d93
branbrin <brandanconde@gmail.com>
2020-10-22 12:42:36 +0200

    prueba 1

--- app/src/main/java/com/example/androidroom/MainActivity.java
+++ app/src/main/java/com/example/androidroom/MainActivity.java
@@ -1,33 +1,126 @@
 package com.example.androidroom;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WordViewModel mWordViewModel;
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mWordViewModel = new ViewModelProvider(this).get(WordViewModel.class);
+        mWordViewModel.getAllWords().observe(this, new Observer<List<Word>>() {
+            @Override
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
+            }
+        });
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
     }
 
 
 
-    @Entity(tableName = "word_table")
-    public class Word {
 
-        @PrimaryKey
-        @NonNull
-        @ColumnInfo(name = "word")
-        private String mWord;
+    public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
-        public Word(String word) {this.mWord = word;}
+        class WordViewHolder extends RecyclerView.ViewHolder {
+            private final TextView wordItemView;
 
-        public String getWord(){return this.mWord;}
+            private WordViewHolder(View itemView) {
+                super(itemView);
+                wordItemView = itemView.findViewById(R.id.textView);
+            }
+        }
+
+        private final LayoutInflater mInflater;
+        private List<Word> mWords; // Cached copy of words
+
+        WordListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+
+        @Override
+        public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+            return new WordViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(WordViewHolder holder, int position) {
+            if (mWords != null) {
+                Word current = mWords.get(position);
+                holder.wordItemView.setText(current.getWord());
+            } else {
+                // Covers the case of data not being ready yet.
+                holder.wordItemView.setText("No Word");
+            }
+        }
+
+        void setWords(List<Word> words){
+            mWords = words;
+            notifyDataSetChanged();
+        }
+
+        // getItemCount() is called many times, and when it is first called,
+        // mWords has not been updated (means initially, it's null, and we can't return null).
+        @Override
+        public int getItemCount() {
+            if (mWords != null)
+                return mWords.size();
+            else return 0;
+        }
     }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            mWordViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getApplicationContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
